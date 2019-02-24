35448b3ed807d1b7b4d24b49f31fab4c54965b64
unknown <anariana111992@gmail.com>
2019-02-25 00:45:21 +0700

    woodgame

---
+++ woodgame.py
@@ -0,0 +1,13 @@
+x = input("Choose Left or Right to get out of wood")
+count = 0
+while count <= 1:
+    if x == "Right":
+        x = input("Choose again Left or Right to get out of the wood")
+        count+=1
+        if count > 1:
+            print("You lose")
+    elif x == "Left":
+        print("You got out of the wood")
+        break
+        
+    
