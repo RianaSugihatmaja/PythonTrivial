d4d66160969e506821bf2b4a06068bf258e4d991
unknown <anariana111992@gmail.com>
2019-02-26 19:35:10 +0700

    fixing bug: if other words are typed

--- woodgame.py
+++ woodgame.py
@@ -1,17 +1,16 @@
 x = input("To get out of the wood, would you go Left or Right? ")
 count = 0
 while count <=2:
-    if x == "Right":
+    if x == "Left":
+        print("You Got Out of The Wood")
+        break
+    else:
         count+=1
         x = input("Which one again? ")
         if count > 3:
             print("You Lose")
-    elif x == "Left":
-        print("You Got Out of The Wood")
-        break
 else:
     if x == "Left":
         print("You Got Out of The Wood")
     else:
-        print("You Lose")        
-    
+        print("You Lose")
